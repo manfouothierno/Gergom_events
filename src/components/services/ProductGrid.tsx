25689f44772a9671fe2b78964196a0544406f45b
manfouothierno <daevintech@gmail.com>
2025-05-20 14:27:40 +0100

    updates

--- src/components/services/ProductGrid.tsx
+++ src/components/services/ProductGrid.tsx
@@ -40,7 +40,7 @@ const ProductGrid = ({ products }: ProductGridProps) => {
 
     return (
         <motion.div
-            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
+            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6"
             variants={container}
             initial="hidden"
             animate="show"
